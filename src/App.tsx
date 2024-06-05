1422d6412b319f84a89886e121c2e23fac7b9912
Roberto Macias <robertomag7@hotmail.com>
2024-06-05 12:25:20 -0700

    hotfix: app subtitle in english

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 export const App: FC = () => {
   return (
     <div className="container mt-3">
-    <h1>Git Issues <small>Seguimiento de problemas</small> </h1>
+    <h1>Git Issues <small>Issues tracking</small></h1>
       <Outlet />
     </div>
   );
