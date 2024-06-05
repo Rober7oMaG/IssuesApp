e9d5c4da810419725ad0e75f8ba40ac57a35e393
Roberto Macias <robertomag7@hotmail.com>
2024-06-05 11:04:11 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import { Outlet } from 'react-router';
+
+export const App: FC = () => {
+  return (
+    <div className="container mt-3">
+    <h1>Git Issues <small>Seguimiento de problemas</small> </h1>
+      <Outlet />
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router';
+import { router } from './router';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
